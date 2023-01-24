eb34f5524b54407b6cd677c1377da2cf49284a16
Isaac <isaac.gz.ov@gmail.com>
2023-01-24 11:57:47 -0600

    chore(token): change get to post

--- src/controllers/token-controller.js
+++ src/controllers/token-controller.js
@@ -1,23 +1,28 @@
+const joi = require('joi');
 const tokenService = require('../services/token-service');
 
+const token = joi.object({
+  mail: joi.string().email().max(100).required(),
+});
+
 const sendMail = async (req, res) => {
-  const {
-    params: { mail },
-  } = req;
-  if (!mail) {
-    res.state(400).send({
+  const result = token.validate(req.body);
+  console.log(req.body.mail);
+  if (result.error) {
+    res.status(400).send({
       status: 'FAILED',
-      data: { error: "Parameter ':mail' can not be empty" },
+      data: { error: result.error.details },
     });
-  }
-  try {
-    await tokenService.sendTokentoMail(mail);
-    res.status(202).send({ status: 'OK', message: 'Mail send' });
-  } catch (error) {
-    res.status(404).send({
-      status: 'FAILED',
-      data: { error: error?.message || error },
-    });
+  } else {
+    try {
+      await tokenService.sendTokentoMail(req.body.mail);
+      res.status(202).send({ status: 'OK', message: 'Mail send' });
+    } catch (error) {
+      res.status(404).send({
+        status: 'FAILED',
+        data: { error: error?.message || error },
+      });
+    }
   }
 };
 
